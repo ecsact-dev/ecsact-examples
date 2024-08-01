fd8969320d7e92e1ae66e5f42c0b94854f1133d5
Kelwan <austin@kelway.ca>
2024-08-01 15:52:56 -0700

    feat: Add Kenny assets, use actualy Ecsact Unity and touchup MovingBlock

--- unity-csharp-basic/Assets/Scripts/Systems/GenerateBlockSystem.cs
+++ unity-csharp-basic/Assets/Scripts/Systems/GenerateBlockSystem.cs
@@ -1,29 +1,32 @@
 using UnityEngine;
 
-public class GenerateBlockSystem : MonoBehaviour {
+public class GenerateBlockSystem : MonoBehaviour
+{
 
   [Ecsact.DefaultSystemImpl(typeof(example.PerformGenerateBlock))]
   public static void
-  PerformGenerateBlock(EcsactRuntime.SystemExecutionContext context) {
+  PerformGenerateBlock(EcsactRuntime.SystemExecutionContext context)
+  {
     var action = context.Action<example.PerformGenerateBlock>();
 
-    context.Add<example.QueueBlock>(
+    context.Add(
         new example.QueueBlock { pos_x = action.pos_x, pos_y = action.pos_y });
   }
 
   [Ecsact.DefaultSystemImpl(typeof(example.GenerateBlock))]
   public static void
-  GenerateBlock(EcsactRuntime.SystemExecutionContext context) {
+  GenerateBlock(EcsactRuntime.SystemExecutionContext context)
+  {
     var builder = context.Generate();
     var block = context.Get<example.QueueBlock>();
 
-    builder.AddComponent<example.CanMove>(new example.CanMove {})
-        .AddComponent<example.Block>(new example.Block {})
-        .AddComponent<example.Position>(
+    builder.AddComponent(new example.CanMove { })
+        .AddComponent(new example.Block { })
+        .AddComponent(
             new example.Position { x = block.pos_x, y = block.pos_y })
-        .AddComponent<example.Collider>(
+        .AddComponent(
             new example.Collider { x_radius = 4, y_radius = 4 })
-        .AddComponent<example.Velocity>(
+        .AddComponent(
             new example.Velocity { x_value = 0, y_value = 0, speed = 0.1f })
         .Finish();
   }
