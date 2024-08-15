5bbef04b5d97becccc501a81b0a3c84da7308569
Ezekiel Warren <ezekiel@seaube.com>
2024-08-15 13:28:09 -0700

    chore: wip

--- unreal-cpp-basic/Source/EcsactUnrealCppBasicEditor.Target.cs
+++ unreal-cpp-basic/Source/EcsactUnrealCppBasicEditor.Target.cs
@@ -6,5 +6,6 @@ public class EcsactUnrealCppBasicEditorTarget : TargetRules {
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("EcsactUnrealCppBasic");
+		ExtraModuleNames.Add("EcsactEditor");
 	}
 }
