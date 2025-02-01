bef3409a1ea1716cf2b995666c5586b4adde0358
Ezekiel Warren <ezekiel@seaube.com>
2025-01-31 16:46:38 -0800

    feat: use si wasmer recipe

--- unreal-cpp-net-fps/Source/EcsactUnrealFps/ExampleCustomEcsactRunnerStarter.cpp
+++ unreal-cpp-net-fps/Source/EcsactUnrealFps/ExampleCustomEcsactRunnerStarter.cpp
@@ -1,6 +1,6 @@
 #include "ExampleCustomEcsactRunnerStarter.h"
 #include "ecsact/runtime/core.h"
-#include "ecsact/wasm.h"
+#include "ecsact/si/wasm.h"
 #include "Ecsact/Private/EcsactGameInstanceSubsystem.h"
 #include "Engine/GameInstance.h"
 #include "EcsactUnreal/EcsactSyncRunner.h"
@@ -19,10 +19,10 @@ static auto LoadWasmSystemImpls() -> void {
 			file_path = FPaths::Combine(FPaths::ProjectDir(), file_path);
 		}
 
-		if(ecsactsi_wasm_load_file != nullptr) {
+		if(ecsact_si_wasm_load_file != nullptr) {
 			// reason for const_casts:
 			// https://github.com/ecsact-dev/ecsact_si_wasm/issues/80
-			auto err = ecsactsi_wasm_load_file(
+			auto err = ecsact_si_wasm_load_file(
 				const_cast<char*>(TCHAR_TO_UTF8(*file_path)),
 				EcsactUnreal::CodegenMeta::ExampleFpsSystemLikeIds.size(),
 				const_cast<ecsact_system_like_id*>(
@@ -32,32 +32,33 @@ static auto LoadWasmSystemImpls() -> void {
 					EcsactUnreal::CodegenMeta::ExampleFpsExportNames.data()
 				)
 			);
-			if(err != ECSACTSI_WASM_OK) {
-#define HANDLE_ECSACTSI_ERROR_CASE(err)                                   \
-	case err:                                                               \
-		UE_LOG(LogTemp, Error, TEXT("ecsactsi_wasm_load_file error: " #err)); \
+			if(err != ECSACT_SI_WASM_OK) {
+#define HANDLE_ECSACT_SI_ERROR_CASE(err)                                   \
+	case err:                                                                \
+		UE_LOG(LogTemp, Error, TEXT("ecsact_si_wasm_load_file error: " #err)); \
 		break
 				switch(err) {
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_FILE_OPEN_FAIL);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_FILE_READ_FAIL);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_COMPILE_FAIL);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_INSTANTIATE_FAIL);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_EXPORT_NOT_FOUND);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_EXPORT_INVALID);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_GUEST_IMPORT_UNKNOWN);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_GUEST_IMPORT_INVALID);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_INITIALIZE_FAIL);
-					HANDLE_ECSACTSI_ERROR_CASE(ECSACTSI_WASM_ERR_NO_SET_SYSTEM_EXECUTION);
-					case ECSACTSI_WASM_OK:
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_FILE_OPEN_FAIL);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_FILE_READ_FAIL);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_COMPILE_FAIL);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_INSTANTIATE_FAIL);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_EXPORT_NOT_FOUND);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_EXPORT_INVALID);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_GUEST_IMPORT_UNKNOWN);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_GUEST_IMPORT_INVALID);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_INITIALIZE_FAIL);
+					HANDLE_ECSACT_SI_ERROR_CASE(ECSACT_SI_WASM_ERR_NO_SET_SYSTEM_EXECUTION
+					);
+					case ECSACT_SI_WASM_OK:
 						break;
 				}
-#undef HANDLE_ECSACTSI_ERROR_CASE
+#undef HANDLE_ECSACT_SI_ERROR_CASE
 			}
 		} else {
 			UE_LOG(
 				LogTemp,
 				Error,
-				TEXT("ecsactsi_wasm_load_file unavailable - cannot load wasm system "
+				TEXT("ecsact_si_wasm_load_file unavailable - cannot load wasm system "
 						 "impls")
 			);
 		}
@@ -86,6 +87,7 @@ auto UExampleCustomEcsactRunnerStarter::StartMultiplayerRunner( //
 	auto ecsact_game_subsystem =
 		world->GetGameInstance()->GetSubsystem<UEcsactGameInstanceSubsystem>();
 	check(ecsact_game_subsystem);
+	LoadWasmSystemImpls();
 	ecsact_game_subsystem->StartCustomRunner<UEcsactAsyncRunner>();
 }
 
